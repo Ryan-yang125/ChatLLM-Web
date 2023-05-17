105e94dd46990fbc44fa71f4e1d39c2ed5510dd2
Ryan-yang125 <ruiyang0012@gmail.com>
2023-05-17 13:24:36 +0800

    refactor: settings

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,10 +1,45 @@
 import React from 'react';
 
+import Image from 'next/image';
 import Link from 'next/link';
 
-import { Settings } from './Settings';
+import { IconAdd, IconInfo, IconSetting } from './Icons';
 
 import { useChatStore } from '@/store/chat';
+
+function BottomSettings() {
+  const chatStore = useChatStore();
+  return (
+    <div className="flex items-center justify-between py-5 relative bottom-0 px-4">
+      <div className="flex">
+        <button
+          onClick={() => chatStore.toggleInstuctionModal(true)}
+          className="btn btn-ghost btn-xs"
+        >
+          <IconInfo />
+        </button>
+        <div className="tooltip" data-tip="in developing...">
+          <button className="btn btn-ghost btn-xs">
+            <IconSetting />
+          </button>
+        </div>
+        <Link
+          href="https://github.com/Ryan-yang125/ChatLLM-Web"
+          target="_blank"
+          className="btn btn-ghost btn-xs"
+        >
+          <Image src="github-mark-white.svg" alt="" width={24} height={24} />
+        </Link>
+      </div>
+      <button
+        onClick={chatStore.newConversation}
+        className="btn btn-ghost btn-xs"
+      >
+        <IconAdd />
+      </button>
+    </div>
+  );
+}
 
 export const ChatItem = (props: {
   onClick?: () => void;
@@ -66,7 +101,7 @@ const Sidebar = () => {
         </ul>
         <div className="from-base-200 pointer-events-none sticky bottom-0 flex h-20 bg-gradient-to-t to-transparent" />
       </div>
-      <Settings />
+      <BottomSettings />
     </div>
   );
 };
